b021576c9c7dabff6c32432b2da0e5839e089150
Lisa <kareevaliza@gmail.com>
2026-04-02 17:31:02 +0100

    added logic to the form

--- ParkingWebApp/Pages/Index.cshtml.cs
+++ ParkingWebApp/Pages/Index.cshtml.cs
@@ -1,20 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ParkingFeeCalculator;
+using static ParkingFeeCalculator.ParkingService;
 
 namespace ParkingWebApp.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public void OnGet()
+        //{
+
+        //}
+
+        [BindProperty]
+        public string VehicleType { get; set; }
+
+        [BindProperty]
+        public int Hours { get; set; }
+
+        public string Result { get; set; }
+
+        //runs when the page is accessed via GET request
+        public void OnGet() { }
+
+        //runs when the form is submitted via POST request
+        //automatically connected to the form due to the method name and attributes
+        public void OnPost()
         {
-            _logger = logger;
+            var service = new ParkingService(new DefaultDiscount());
+            var fee = service.CalculateFee(Hours, VehicleType);
+            Result = $"Fee: €{fee}";
         }
 
-        public void OnGet()
+        //fake the discount service for demonstration purposes
+        public class DefaultDiscount : IDiscountService
         {
-
+            public double GetDiscount()
+            {
+                return 0.9; // 10% discount
+            }
         }
     }
 }
