64b95240c74e2f0b5e573e030c0eea4a758c8a2c
Lisa <kareevaliza@gmail.com>
2026-04-21 21:52:29 +0100

    changed unit tests

--- ParkingFeeCalculator.Tests/UnitTest1.cs
+++ ParkingFeeCalculator.Tests/UnitTest1.cs
@@ -19,80 +19,98 @@ namespace ParkingFeeCalculator.Test
             _service = new ParkingService(_mockDiscount.Object);
         }
 
-
+        /// <summary>
+        /// TC1: 3 hours, standard vehicle
+        /// Expected fee: 12.00
+        /// NOTE: This test exposes a CODE BUG - the condition (hours >= 1) && (hours < 3) 
+        /// does NOT include 3 hours. Hours=3 falls into a gap and currently returns incorrect fee.
+        /// The condition should be changed to (hours >= 1) && (hours <= 3) to fix this.
+        /// Branches covered ideally: N3=T, N4=T
+        /// </summary>
         [Test]
         public void TC1()
         {
             var result = _service.CalculateFee(3, "standard");
-
             Assert.That(result, Is.EqualTo(12.0));
             _mockDiscount.Verify(d => d.GetDiscount(), Times.Once);
         }
 
+        /// <summary>
+        /// TC2: 10 hours, standard vehicle
+        /// Expected fee: 21.00* (includes 10% discount)
+        /// Branches covered: N3=T, N4=F, N6=T, N20=T
+        /// </summary>
         [Test]
         public void TC2()
         {
-            var result = _service.CalculateFee(5, "standard");
-
-            Assert.That(result, Is.EqualTo(15.0));
+            var result = _service.CalculateFee(10, "standard");
+            Assert.That(result, Is.EqualTo(27.0));
             _mockDiscount.Verify(d => d.GetDiscount(), Times.Once);
         }
 
+        /// <summary>
+        /// TC3: 0 hours, standard vehicle
+        /// Expected fee: 0
+        /// Branches covered: N3=T, N4=F, N6=F, N20=F
+        /// </summary>
         [Test]
         public void TC3()
         {
             var result = _service.CalculateFee(0, "standard");
-
             Assert.That(result, Is.EqualTo(0.0));
             _mockDiscount.Verify(d => d.GetDiscount(), Times.Once);
         }
 
+        /// <summary>
+        /// TC4: 5 hours, truck vehicle
+        /// Expected fee: 0
+        /// Branches covered: N3=F, N10=F, N11=F, N20=F
+        /// </summary>
         [Test]
         public void TC4()
         {
-            var result = _service.CalculateFee(3, "electric");
-
-            Assert.That(result, Is.EqualTo(9.0));
+            var result = _service.CalculateFee(5, "truck");
+            Assert.That(result, Is.EqualTo(0.0));
             _mockDiscount.Verify(d => d.GetDiscount(), Times.Once);
         }
 
+        /// <summary>
+        /// TC5: 5 hours, electric vehicle
+        /// Expected fee: 15.00
+        /// Branches covered: N3=F, N10=T, N11=T
+        /// </summary>
         [Test]
         public void TC5()
         {
-            var result = _service.CalculateFee(7, "electric");
+            var result = _service.CalculateFee(5, "electric");
+            Assert.That(result, Is.EqualTo(15.0));
+            _mockDiscount.Verify(d => d.GetDiscount(), Times.Once);
+        }
 
+        /// <summary>
+        /// TC6: 7 hours, electric vehicle
+        /// Expected fee: 14.00
+        /// Branches covered: N3=F, N10=T, N11=F, N13=T
+        /// </summary>
+        [Test]
+        public void TC6()
+        {
+            var result = _service.CalculateFee(7, "electric");
             Assert.That(result, Is.EqualTo(14.0));
             _mockDiscount.Verify(d => d.GetDiscount(), Times.Once);
         }
 
-        [Test]
-        public void TC6()
-        {
-            var result = _service.CalculateFee(0, "electric");
-
-            Assert.That(result, Is.EqualTo(0.0));
-            _mockDiscount.Verify(d => d.GetDiscount(), Times.Once);
-        }
-
+        /// <summary>
+        /// TC7: 0 hours, electric vehicle
+        /// Expected fee: 0
+        /// Branches covered: N3=F, N10=T, N11=F, N13=F, N20=F
+        /// </summary>
         [Test]
         public void TC7()
         {
-            var result = _service.CalculateFee(5, "truck");
-
+            var result = _service.CalculateFee(0, "electric");
             Assert.That(result, Is.EqualTo(0.0));
             _mockDiscount.Verify(d => d.GetDiscount(), Times.Once);
         }
-
-        [Test]
-        public void TC8()
-        {
-            var result = _service.CalculateFee(12, "standard");
-
-            Assert.That(result, Is.EqualTo(32.4));
-            _mockDiscount.Verify(d => d.GetDiscount(), Times.Once);
-        }
-
-
-
     }
 }
